e4dc787031735f092bde7a7558757e6596d8e805
Russ Tennant <russ@venturetech.net>
2016-07-27 16:26:32 -0500

    Synchronized live site with webdev project
    Updated to be based on proteus-examples webdev project
    where feasible.

---
+++ README.md
@@ -0,0 +1,8 @@
+# vendorproof-webdev
+
+VendorProof Application Web Development
+
+To get up and running - from the project directory run ...
+
+- npm install
+- bower install
--- gulpfile.js
+++ gulpfile.js
@@ -1,61 +1,179 @@
 var gulp = require('gulp');
+var gutil = require('gulp-util');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var minifyCss = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 var clip = require('gulp-clip-empty-files');
+var sourcemaps = require("gulp-sourcemaps");
+var uglify = require('gulp-uglify');
+var del = require('del');
+var include = require("gulp-include");
+var favicons = require('gulp-favicons');
+var runSequence = require('run-sequence');
 var iconfont = require('gulp-iconfont');
 var consolidate = require('gulp-consolidate');
-var rename = require('gulp-rename');
+var flatten = require('gulp-flatten');
 var _ = require('lodash');
-var del = require('del');
 
+gulp.task('default', ['build']);
 
-gulp.task('default', ['styles']);
-
+gulp.task('build', ['styles', 'design', 'javascript', 'favicons']);
+gulp.task('clean', function(callback) {
+    del(['./build/']).then(function(data) {
+        callback();
+    });
+});
+gulp.task('styles', function (callback) {
+    runSequence('styles:clean', ['styles:build', 'styles:vendor'], callback);
+});
+gulp.task('styles:build', function() {
+    return gulp.src('./web/src/stylesheets/**/*.scss')
+        .pipe(clip())
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(autoprefixer({
+            browsers: ['> 1%', 'last 2 versions', 'ie > 9'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('./build/stylesheets'))
+        .pipe(minifyCss())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./build/stylesheets'))
+        ;
+});
+gulp.task('styles:vendor', function() {
+    return gulp.src(['./bower_components/select2/dist/css/select2.css'
+    ])
+        .pipe(clip())
+        .pipe(gulp.dest('./build/stylesheets/vendor'))
+        .pipe(minifyCss())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('./build/stylesheets/vendor'))
+        ;
+});
 gulp.task('styles:clean', function(callback) {
-	del(['./web/stylesheets/build/**'], callback);
+    del(['./build/stylesheets']).then(function(data) {
+        callback();
+    });
 });
 
-gulp.task('styles:copy-css', ['styles:clean'], function(){
-	return gulp.src('./web/stylesheets/src/**/*.css')
-		.pipe(gulp.dest('./web/stylesheets/build'));
+gulp.task('javascript', function(callback){
+    runSequence('javascript:clean', ['javascript:build', 'javascript:copy-min'/*, 'javascript:vendor'*/], callback);
+});
+gulp.task('javascript:build', function() {
+    return gulp.src(['./web/src/javascript/**/*.js', '!./web/src/javascript/components/*.js',
+            '!./web/src/javascript/**/*.min.js'])
+        .pipe(clip())
+        .pipe(include())
+            .on('error', console.log)
+        .pipe(gulp.dest('./build/javascript'))
+        .pipe(sourcemaps.init({ debug: true }))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('../sourcemaps/', {
+            sourceMappingURLPrefix: '/_sourcemaps'
+        }))
+        .pipe(flatten({includeParents: 2}))
+        .pipe(gulp.dest('./build/javascript'))
+        ;
+});
+gulp.task('javascript:copy-min', function() {
+    return gulp.src(['./web/src/javascript/**/*.min.js'])
+        .on('error', console.log)
+        .pipe(gulp.dest('./build/javascript'))
+        ;
+});
+/*gulp.task('javascript:vendor', function() {
+    return gulp.src(['./bower_components/jquery/dist/jquery.js',
+            './bower_components/tether/dist/js/tether.js',
+            './bower_components/select2/dist/js/select2.js',
+            './bower_components/select2/dist/js/!**!/i18n/!*.js', // globing for directory creation
+            './bower_components/bootstrap/dist/js/bootstrap.js'
+    ])
+        .pipe(gulp.dest('./build/javascript/vendor'))
+        .pipe(sourcemaps.init({ debug: true }))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(sourcemaps.write('../sourcemaps/', {
+            sourceMappingURLPrefix: '/_sourcemaps'
+        }))
+        .pipe(flatten({includeParents: 2}))
+        .pipe(gulp.dest('./build/javascript/vendor'))
+        ;
+});*/
+gulp.task('javascript:clean', function(callback) {
+    del(['./build/javascript', './build/sourcemaps']).then(function(data) {
+        callback();
+    });
 });
 
-gulp.task('styles:build-scss', ['styles:copy-css'], function() {
-	return gulp.src('./web/stylesheets/src/**/*.scss')
-		.pipe(clip())
-		.pipe(sass({
-			outputStyle: 'expanded'
-		}))
-		.pipe(autoprefixer({
-			browsers: ['> 1%', 'last 2 versions', 'ie >= 8'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('./web/stylesheets/build'));
+gulp.task('design', function (callback) {
+    runSequence('design:clean', ['design:build'], callback);
+});
+gulp.task('design:build', function() {
+    return gulp.src('./web/src/design/**/*')
+        .pipe(gulp.dest('./build/design'));
+});
+gulp.task('design:clean', function(callback) {
+    del(['./build/design']).then(function(data) {
+        callback();
+    });
+});
+
+gulp.task('favicons', function (callback) {
+    runSequence('favicons:clean', ['favicons:build'], callback);
+});
+gulp.task('favicons:build', function() {
+    return gulp.src("./web/src/favicons/logo.png")
+        .pipe(favicons({
+            appName: "VendorProof",
+            appDescription: "Healthcare Vendor Compliance Made Easy",
+            developerName: "ProviderTrust",
+            developerURL: "https://www.vendorproof.com/",
+            background: "#FFFFFF",
+            path: "/_favicons/",
+            url: "/",
+            display: "standalone",
+            orientation: "portrait",
+            version: 1.0,
+            logging: false,
+            online: false,
+            html: "index.html",
+            pipeHTML: true,
+            replace: true
+        }))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('./build/favicons'));
+});
+gulp.task('favicons:clean', function(callback) {
+    del(['./build/favicons']).then(function(data) {
+        callback();
+    });
 });
 
 gulp.task('iconfont', function(){
-	return gulp.src('./font-glyphs/src/icons/*.svg')
-		.pipe(iconfont({
-			fontName: 'vp-glyph-font', // required
-			appendCodepoints: true, // recommended option
-			startCodepoint: 0xF101,
-			fontHeight: 150
-		}))
-		.on('codepoints', function(codepoints, options) {
-			codepoints = _.map(codepoints, function(codepoint) {
-				return {
-					name: codepoint.name,
-					codepoint: codepoint.codepoint.toString(16).toLowerCase()
-				};
-			});
-			gulp.src('./font-glyphs/src/templates/_iconfont.scsstpl')
-				.pipe(consolidate('lodash', {
-					glyphs: codepoints
-				}))
-				.pipe(rename('_font-glyph-entities.scss'))
-				.pipe(gulp.dest('./web/stylesheets/src/config/'));
-		})
-		.pipe(gulp.dest('./web/design/fonts/glyph-lib/'));
+    return gulp.src('./font-glyphs/src/icons/*.svg')
+        .pipe(iconfont({
+            fontName: 'vp-glyph-font', // required
+            appendCodepoints: true, // recommended option
+            startCodepoint: 0xF101,
+            fontHeight: 150
+        }))
+        .on('codepoints', function(codepoints, options) {
+            codepoints = _.map(codepoints, function(codepoint) {
+                return {
+                    name: codepoint.name,
+                    codepoint: codepoint.codepoint.toString(16).toLowerCase()
+                };
+            });
+            gulp.src('./font-glyphs/src/templates/_iconfont.scsstpl')
+                .pipe(consolidate('lodash', {
+                    glyphs: codepoints
+                }))
+                .pipe(rename('_font-glyph-entities.scss'))
+                .pipe(gulp.dest('./build/stylesheets/src/config/'));
+        })
+        .pipe(gulp.dest('./build/design/fonts/glyph-lib/'));
 });
-
-gulp.task('styles', ['styles:build-scss']);
